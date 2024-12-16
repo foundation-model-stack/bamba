4648c5d6c081e0bc17711e4ab2ae18f79a21a4ca
Yotam Perlitz <y.perlitz@ibm.com>
2024-12-16 11:35:05 +0200

    improve model importing from s3
    Signed-off-by: Yotam Perlitz <y.perlitz@ibm.com>

--- evaluation/get_model_from_s3.py
+++ evaluation/get_model_from_s3.py
@@ -1,6 +1,9 @@
-import os
 import argparse
+import os
+
 import boto3
+from loguru import logger
+
 
 def download_from_s3(prefix, local_path, bucket_name, endpoint_url):
     """Downloads files from an S3 bucket to a local directory.
@@ -12,11 +15,13 @@ def download_from_s3(prefix, local_path, bucket_name, endpoint_url):
         endpoint_url (str): The endpoint URL of the S3 service.
     """
 
-    aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
-    aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    aws_access_key_id = os.environ.get("ACCESS_KEY_ID")
+    aws_secret_access_key = os.environ.get("SECRET_ACCESS_KEY")
 
     if not aws_access_key_id or not aws_secret_access_key:
-        raise ValueError("AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY must be set as environment variables.")
+        raise ValueError(
+            "ACCESS_KEY_ID and SECRET_ACCESS_KEY must be set as environment variables."
+        )
 
     s3 = boto3.client(
         "s3",
@@ -31,15 +36,29 @@ def download_from_s3(prefix, local_path, bucket_name, endpoint_url):
         for obj in page.get("Contents", []):
             key = obj["Key"]
             local_file_path = os.path.join(local_path, os.path.relpath(key, prefix))
+            if os.path.exists(local_file_path):
+                logger.info(f"{local_file_path} already exists, skipping...")
+                continue
+
             os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
             s3.download_file(bucket_name, key, local_file_path)
+            logger.info(f"Downloaded {key} to {local_file_path}")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download files from S3.")
-    parser.add_argument("--prefix", required=True, help="Prefix of the objects to download in the S3 bucket.")
-    parser.add_argument("--local_path", required=True, help="Local directory to download the files to.")
+    parser.add_argument(
+        "--prefix",
+        required=True,
+        help="Prefix of the objects to download in the S3 bucket.",
+    )
+    parser.add_argument(
+        "--local_path", required=True, help="Local directory to download the files to."
+    )
     parser.add_argument("--bucket_name", required=True, help="Name of the S3 bucket.")
-    parser.add_argument("--endpoint_url", required=True, help="Endpoint URL of the S3 service.")
+    parser.add_argument(
+        "--endpoint_url", required=True, help="Endpoint URL of the S3 service."
+    )
 
     args = parser.parse_args()
 
